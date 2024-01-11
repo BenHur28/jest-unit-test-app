6a23675e6571aa987496da8ab7b23be3c99f6d0c
Ben Hur <bhur777@gmail.com>
2024-01-11 14:58:16 -0500

    make failed tests into passing

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ export default function Page() {
 	return (
 		<div>
 			<h1>Home</h1>
+			<button disabled>Button</button>
+			<p data-testid="paragraph-blue" className="blue">
+				Red Text
+			</p>
 		</div>
 	);
 }
