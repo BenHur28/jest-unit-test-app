60c4a7df0fc1565797bfb88c006ef948db16ff1a
Ben Hur <bhur777@gmail.com>
2024-01-11 14:46:48 -0500

    add more tests

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -11,4 +11,20 @@ describe("Page", () => {
 
 		expect(heading).toHaveTextContent(headerText);
 	});
+
+	it("Should have a disabled button", () => {
+		render(<Page />);
+
+		const buttonElement = screen.getByRole("button");
+
+		expect(buttonElement).toBeDisabled();
+	});
+
+	it("Should have a className of blue", () => {
+		render(<Page></Page>);
+
+		const pElement = screen.getByTestId("paragraph-blue");
+
+		expect(pElement).toHaveClass("blue");
+	});
 });
